b0b29541fcd58e79747f1619e7c01f9cff3b9003
Maxime Morin <maxime_morette@hotmail.com>
2023-04-24 03:35:07 -0400

    feat : Base Controller and subobjects

---
+++ Source/Battlebots/Private/Bot.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bot.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+// Sets default values
+ABot::ABot()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	//Create our components
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootSceneComponent"));
+	StaticMeshComp = CreateDefaultSubobject <UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+
+	//Attach our components
+	StaticMeshComp->SetupAttachment(RootComponent);
+	SpringArmComp->SetupAttachment(StaticMeshComp);
+	CameraComp->SetupAttachment(SpringArmComp,USpringArmComponent::SocketName);
+
+	//Assign SpringArm class variables.
+	SpringArmComp->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, 50.0f), FRotator(-60.0f, 0.0f, 0.0f));
+	SpringArmComp->TargetArmLength = 400.f;
+	SpringArmComp->bEnableCameraLag = true;
+	SpringArmComp->CameraLagSpeed = 3.0f;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("Ability System");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	//Take control of the default Player
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	AutoPossessAI = EAutoPossessAI::Disabled;
+	AIControllerClass = nullptr;
+}
+
+// Called when the game starts or when spawned
+void ABot::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+// void ABot::Tick(float DeltaTime)
+// {
+// 	Super::Tick(DeltaTime);
+// }
+
+// Called to bind functionality to input
+void ABot::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+UAbilitySystemComponent* ABot::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+
+
---
+++ Source/Battlebots/Public/Bot.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GameplayAbilities/Public/AbilitySystemInterface.h"
+#include "GameplayAbilities/Public/AbilitySystemComponent.h"
+#include "Bot.generated.h"
+
+UCLASS()
+class BATTLEBOTS_API ABot : public APawn, public IAbilitySystemInterface
+{
+private:
+	GENERATED_BODY()
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	class USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(EditAnywhere)
+	class UCameraComponent* CameraComp;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* StaticMeshComp;
+
+	/** Ability System Component. Required to use Gameplay Attributes and Gameplay Abilities. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Abilities")
+	UAbilitySystemComponent* AbilitySystemComponent;
+
+public:
+	// Sets default values for this pawn's properties
+	ABot();
+	
+	// Called every frame
+	//virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	//~ Begin IAbilitySystemInterface
+	/** Returns our Ability System Component. */
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	//~ End IAbilitySystemInterface
+};
